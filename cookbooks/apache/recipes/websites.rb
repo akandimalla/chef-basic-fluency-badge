fd33ac330808ebf84daa39405e8706653e7a9f38
Ashok <ashok.k9959@gmail.com>
2017-06-21 11:09:10 -0700

    adding index.html in websites.rb

---
+++ cookbooks/apache/recipes/websites.rb
@@ -0,0 +1,12 @@
+file '/var/www/index.html' do
+        action :delete
+
+end 
+
+file '/var/www/html/index.html' do 
+        content 'This is apache web server-v.0.1.0'
+        mode '0755'
+        owner 'root'
+        group 'apache'
+
+end
