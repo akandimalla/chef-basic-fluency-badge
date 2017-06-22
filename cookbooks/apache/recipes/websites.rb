adc96fe4ddefa2ef84a527afb23929e8d9dc41d3
Ashok <ashok.k9959@gmail.com>
2017-06-22 11:38:09 -0700

    install and run  postgresql service

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,13 +1,4 @@
-
-file '/var/www/index.html' do
-        action :delete
-
-end 
-
-file '/var/www/html/index.html' do 
-        content 'This is apache web server-v.0.1.0'
-        mode '0755'
-        owner 'root'
-        group 'apache'
-
+file 'default www' do
+	path '/var/www/html/index.html'
+	content 'Helloworld!!!'
 end
